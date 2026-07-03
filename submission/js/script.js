2f92d8f7991a7f9ad8c872a9b7bb4bca72827a03
wamuyu-debug <lisangacha6@gmail.com>
2026-07-03 13:34:12 +0300

    Finalize portfolio, deploy configuration, and documentation

--- submission/js/script.js
+++ submission/js/script.js
@@ -165,4 +165,38 @@ learnMoreBtn.addEventListener("click", () => {
 
     }
 
+})
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+    if(document.documentElement.scrollTop > 300){
+
+        topBtn.style.display = "block";
+
+    }else{
+
+        topBtn.style.display = "none";
+
+    }
+
+}
+
+topBtn.onclick = function(){
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+}
+const topBtn = document.getElementById("topBtn");
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
